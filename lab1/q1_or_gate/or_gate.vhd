7830d1a1f376fbffe06546c0757d8b20ac37ab66
Ernane Ferreira <ernane.junior25@gmail.com>
2022-09-04 14:46:42 -0300

    Implementing question 1 - or gate

--- lab1/q1_or_gate/or_gate.vhd
+++ lab1/q1_or_gate/or_gate.vhd
@@ -1,13 +1,13 @@
--- Entidade
 entity or_gate is
-	port (
-		a	: in bit;
-		b 	: in bit;
-		s	: out bit
+	port(
+		a, b : in bit;
+		s    : out bit
 	);
 end or_gate;
 
-architecture top of or_gate is
-begin 
+architecture main of or_gate is
+begin
+
 	s <= a or b;
-end top;
+	
+end architecture main;
--- lab1/q1_or_gate/simulation/qsim/work/or_gate_vlg_sample_tst/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity or_gate_vlg_sample_tst is
-    port(
-        a               : in     vl_logic;
-        b               : in     vl_logic;
-        sampler_tx      : out    vl_logic
-    );
-end or_gate_vlg_sample_tst;
