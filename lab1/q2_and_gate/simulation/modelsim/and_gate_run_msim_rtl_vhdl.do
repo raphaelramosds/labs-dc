25a42b02037514482963ee6667832187d95da143
Ernane Ferreira <ernane.junior25@gmail.com>
2022-09-04 15:01:18 -0300

    Implementing simulation and partial documentation for question 2 - and gate

---
+++ lab1/q2_and_gate/simulation/modelsim/and_gate_run_msim_rtl_vhdl.do
@@ -0,0 +1,9 @@
+transcript on
+if {[file exists rtl_work]} {
+	vdel -lib rtl_work -all
+}
+vlib rtl_work
+vmap work rtl_work
+
+vcom -93 -work work {C:/Users/ernan/OneDrive/Área de Trabalho/vhdl/and_gate/and_gate.vhd}
+
--- lab1/q2_and_gate/simulation/qsim/work/and_gate/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity and_gate is
-    port(
-        a               : in     vl_logic;
-        b               : in     vl_logic;
-        s               : out    vl_logic
-    );
-end and_gate;
