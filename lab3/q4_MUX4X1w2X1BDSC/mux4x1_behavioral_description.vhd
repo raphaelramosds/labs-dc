e143b3da18019894900d88967706ce561c927a8b
raphaelramosds <raphael.opensource@gmail.com>
2022-10-03 11:14:34 -0300

    fix: setting i0 to LSB

--- lab3/q4_MUX4X1w2X1BDSC/mux4x1_behavioral_description.vhd
+++ lab3/q4_MUX4X1w2X1BDSC/mux4x1_behavioral_description.vhd
@@ -1,23 +1,44 @@
 entity mux4x1_behavioral_description is
   port(
-    I0, I1, I2, I3 : in bit; -- Inputs
-    s0_in, s1_in    : in bit; -- Signals
-    S              : out bit -- Output
+  
+	 -- Inputs
+    I0, I1, I2, I3 : in bit; 
+	 
+	 -- Signals
+    s0_in, s1_in : in bit; 
+	 
+	 -- Output
+    S : out bit 
+	 
   );
 end mux4x1_behavioral_description;
 
 architecture behav of mux4x1_behavioral_description is
-  signal S0: bit;
-  signal S1: bit;
+
+  signal D0: bit;
+  signal D1: bit;
+  
+  -- 2X1 Mux 
+  
   component Mux2x1BehavioralDescription is
     port(
-      I0, I1, s_in : in bit;
+      I0, I1, s_in : in bit; -- I0 : LSB
       S            : out bit
     );
   end component;
   
 begin
-  u1 : Mux2x1BehavioralDescription port map(I0 => I0, I1 => I1, s_in => s1_in, S => S0);
-  u2 : Mux2x1BehavioralDescription port map(I0 => I2, I1 => I3, s_in => s1_in, S => S1);
-  u3 : Mux2x1BehavioralDescription port map(I0 => S0, I1 => S1, s_in => s0_in, S => S);
+	
+	-- 1° MUX recebe I0 e I1, om seletor s0_in
+	
+	u1 : Mux2x1BehavioralDescription port map(I0 => I0, I1 => I1, s_in => s0_in, S => D0);
+	
+	-- 2° MUX recebe I2 e I3, com seletor s0_in
+	
+	u2 : Mux2x1BehavioralDescription port map(I0 => I2, I1 => I3, s_in => s0_in, S => D1);
+	
+	-- Último MUX recebe D0 e D1, com seletor s1_in
+	
+	u3 : Mux2x1BehavioralDescription port map(I0 => D0, I1 => D1, s_in => s1_in, S => S);
+	
 end architecture;
