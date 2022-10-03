fe6ef5e7d653c79639a12e1861627738f729a572
raphaelramosds <raphael.opensource@gmail.com>
2022-10-03 11:23:39 -0300

    fix: setting s0 to LSB

--- lab3/q4_MUX4X1w2X1BDSC/mux4x1_behavioral_description.vhd
+++ lab3/q4_MUX4X1w2X1BDSC/mux4x1_behavioral_description.vhd
@@ -1,44 +1,44 @@
-entity mux4x1_behavioral_description is
-  port(
-  
-	 -- Inputs
-    I0, I1, I2, I3 : in bit; 
-	 
-	 -- Signals
-    s0_in, s1_in : in bit; 
-	 
-	 -- Output
-    S : out bit 
-	 
+ENTITY mux4x1_behavioral_description IS
+  PORT (
+
+    -- Inputs
+    I0, I1, I2, I3 : IN BIT;
+
+    -- Signals
+    s0_in, s1_in : IN BIT;
+
+    -- Output
+    S : OUT BIT
+
   );
-end mux4x1_behavioral_description;
+END mux4x1_behavioral_description;
 
-architecture behav of mux4x1_behavioral_description is
+ARCHITECTURE behav OF mux4x1_behavioral_description IS
 
-  signal D0: bit;
-  signal D1: bit;
-  
+  SIGNAL D0 : BIT;
+  SIGNAL D1 : BIT;
+
   -- 2X1 Mux 
-  
-  component Mux2x1BehavioralDescription is
-    port(
-      I0, I1, s_in : in bit; -- I0 : LSB
-      S            : out bit
+
+  COMPONENT Mux2x1BehavioralDescription IS
+    PORT (
+      I0, I1, s_in : IN BIT; -- I0 : LSB
+      S : OUT BIT
     );
-  end component;
-  
-begin
-	
-	-- 1° MUX recebe I0 e I1, om seletor s0_in
-	
-	u1 : Mux2x1BehavioralDescription port map(I0 => I0, I1 => I1, s_in => s0_in, S => D0);
-	
-	-- 2° MUX recebe I2 e I3, com seletor s0_in
-	
-	u2 : Mux2x1BehavioralDescription port map(I0 => I2, I1 => I3, s_in => s0_in, S => D1);
-	
-	-- Último MUX recebe D0 e D1, com seletor s1_in
-	
-	u3 : Mux2x1BehavioralDescription port map(I0 => D0, I1 => D1, s_in => s1_in, S => S);
-	
-end architecture;
+  END COMPONENT;
+
+BEGIN
+
+  -- 1° MUX recebe I0 e I1, om seletor s0_in
+
+  u1 : Mux2x1BehavioralDescription PORT MAP(I0 => I0, I1 => I1, s_in => s0_in, S => D0);
+
+  -- 2° MUX recebe I2 e I3, com seletor s0_in
+
+  u2 : Mux2x1BehavioralDescription PORT MAP(I0 => I2, I1 => I3, s_in => s0_in, S => D1);
+
+  -- Último MUX recebe D0 e D1, com seletor s1_in
+
+  u3 : Mux2x1BehavioralDescription PORT MAP(I0 => D0, I1 => D1, s_in => s1_in, S => S);
+
+END ARCHITECTURE;
